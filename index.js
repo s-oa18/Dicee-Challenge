05f54053434aab61c19f149098864ac23baca1e6
SethAmpofo <99514561+s-oa18@users.noreply.github.com>
2022-08-03 01:11:45 +0000

    update

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
 var randomNumber1 = Math.floor(Math.random() * 6) + 1;
-console.log(randomNumber1);
+var randomDiceImage = "Images/dice" + randomNumber1 + ".png";
+
+var firstImage = document.querySelectorAll("img")[0];
+var changeImage = firstImage.setAttribute("src", randomDiceImage);
+
+var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+var randomDiceImage2 = "Images/dice" + randomNumber2 + ".png";
+var secondImage = document.querySelectorAll("img")[1];
+
+secondImage.setAttribute("src", randomDiceImage2);
+
+If(randomNumber1 > randomNumber2);
+{
+  document.querySelector("h1").innerHTML = "Player 1 Wins 🏆";
+}
